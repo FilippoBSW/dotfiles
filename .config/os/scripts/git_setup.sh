0a2b2bcee910b9f3be038eb3b1b06369d60498b6
FilippoBSW <filippobsw@gmail.com>
2024-10-10 12:44:22 +0900

    Commit

---
+++ .config/os/scripts/git_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git config --global user.name "$1"
+git config --global user.email "$2"
+git config --global core.editor "emacsclient -nw"
+git config --global init.defaultBranch main
+cat ~/.config/delta/config >> ~/.gitconfig
+ssh-keygen -t ed25519 -C "$2"
